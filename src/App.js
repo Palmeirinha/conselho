836c1ba6b224c0aec39746fd0b4fafd352d1b74d
João Pedro O Pereira <141459770+Palmeirinha@users.noreply.github.com>
2024-08-15 22:46:49 -0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-// src/App.js
 import React from 'react';
 import './App.css';
-import Service from './service'; // Atualize o caminho para o novo nome do arquivo
+import Service from './service'; 
 
 function App() {
+  const handleReload = () => {
+    window.location.reload();
+  };
+
   return (
     <div className="App">
+      <h1>Advice of the day</h1>
       <Service />
+      <div className="reload">
+        <button onClick={handleReload} className="reload-button">Reload the page for more advice</button>
+      </div>
     </div>
   );
 }
